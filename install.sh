58bcfff8363f45d81b246df29e3848ea04e44607
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2021-12-26 17:15:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,15 +8,21 @@ cd "debianzorinosinstall"
 echo "downloading"
 curl -LO "${URL}/z/zorin-desktop-themes/zorin-desktop-themes_3.4.2_all.deb"
 curl -LO "${URL}/z/zorin-icon-themes/zorin-icon-themes_2.9.4_all.deb"
+curl -LO "${URL}/z/zorin-os-wallpapers/zorin-os-wallpapers/zorin-os-wallpapers-16_16.2_all.deb"
 curl -LO "${URL}/z/zorin-os-wallpapers/zorin-os-wallpapers/zorin-os-wallpapers_16.2_all.deb"
-curl -LO "${URL}/z/zorin-os-lite-default-settings/zorin-os-lite-default-settings_16.1.4_all.deb"
+curl -LO "${URL}/z/zorin-os-lite-default-settings/zorin-os-lite-default-settings_16.1.4.tar.xz"
 curl -LO "${URL}/x/xfce4-zorinmenulite-plugin/xfce4-zorinmenulite-plugin_1.1.4_${ARCH}.deb"
 
 sudo dpkg -i "zorin-desktop-themes_3.4.2_all.deb"
 sudo dpkg -i "zorin-icon-themes_2.9.4_all.deb"
+sudo dpkg -i "zorin-os-wallpaper-16_16.2_all.deb"
 sudo dpkg -i "zorin-os-wallpaper_16.2_all.deb"
-sudo dpkg -i "zorin-os-lite-default-settings_16.1.4_all.deb"
 sudo dpkg -i "xfce4-zorinmenulite-plugin_1.1.4_${ARCH}.deb"
+
+xz -d -v zorin-os-lite-default-settings_16.1.4.tar.xz
+tar -xf zorin-os-lite-default-settings_16.1.4.tar
+cd zorin-os-lite-default-settings_16.1.4/
+sudo make DESTDIR=/ install
 
 cd ..
 rm -rf "debianzorinosinstall/"
